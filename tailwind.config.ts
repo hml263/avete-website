b50298716759c2966a25335c9d65078335848d49
hml078126 <htetmyetlwin995@gmail.com>
2024-07-04 16:36:57 +0700

    Added framer motion and react scroll plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,24 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+      padding: "1rem",
+    },
+    screens: {
+      sm: "660px",
+      md: "760px",
+      lg: "960px",
+      xl: "1200px",
     },
     extend: {
+      colors: {
+        primary: "#16213F",
+        secondary: "#050D28",
+        lighted: "#EFEFEF",
+        blue: {
+          DEFAULT: "#336DFF",
+          hover: "336DFF",
+        },
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
