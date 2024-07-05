ae62a76f6c912ef2f9c57770aad40baa08eb94d6
hml078126 <htetmyetlwin995@gmail.com>
2024-07-05 17:15:20 +0700

    Added text-animation in Hero section

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,10 +1,21 @@
 'use client'
 
+import React from 'react';
 import { Download } from 'lucide-react'
 import Link from 'next/link'
-import { TypeAnimation } from 'react-type-animation';
+import TextTransition, { presets } from 'react-text-transition';
 
+const TEXTS = ['Safety', 'Productivity', 'Training'];
 const Hero = () => {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      3000, // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
   return (
    <section className='w-f pt-40'>
         <div className='container mx-auto h-full'>
@@ -13,21 +24,10 @@ const Hero = () => {
                 <div className='text-center xl:text-left'>
                     <span className='text-[20px] font-medium leading-[2rem]'>Enhancing</span><br />
                     <span className='text-[20px] font-medium leading-[2rem]'>Construction</span><br />
-                    <TypeAnimation 
-      sequence={[
-        'Safety',
-        1000,
-        'Productivity',
-        1000,
-        'Training',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2rem', fontWeight: 'bold', display: 'inline-block', color: '#DAEE50', textTransform: 'uppercase' }}
-      repeat={Infinity}
-    /><br />
-                        <span className='text-[20px] font-medium leading-[2rem]'>Next Gen AI</span><br />
+                    <h1 className="text-yellow text-[30px] uppercase font-bold">
+      <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
+    </h1>
+                        <span className='text-[20px] font-medium '>Next Gen AI</span><br />
                         <div className='py-6'>
                             <p>
                             Revolutionize construction sites with our AI 
